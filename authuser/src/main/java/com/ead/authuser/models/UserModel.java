038455c82df0792b7a836a3e1f26bf8124e9e691
Railton <railtonplayrx@gmail.com>
2025-12-24 09:44:01 -0300

    Add UserModel and UserRepository for user management functionality

---
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -0,0 +1,77 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.UUID;
+
+
+@Data
+@Table(name = "TB_USERS")
+@Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false,length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String fullName;
+
+    @Column(nullable = false, length = 20)
+    private String cpf;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy:mm:ss")
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy:mm:ss")
+    private LocalDateTime lastUpdatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        creationDate = LocalDateTime.now(ZoneId.of("UTC"));
+        lastUpdatedDate = LocalDateTime.now(ZoneId.of("UTC"));
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastUpdatedDate = LocalDateTime.now(ZoneId.of("UTC"));
+    }
+}
