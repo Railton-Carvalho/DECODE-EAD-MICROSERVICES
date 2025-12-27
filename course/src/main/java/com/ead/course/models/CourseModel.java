5ec331c4cc2f8cb5b139d692bfa98fe2b48950c6
Railton <railtonplayrx@gmail.com>
2025-12-27 11:02:50 -0300

    Add CourseModel, ModuleModel, and LessonModel with relationships and JSON properties for course management

--- course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/src/main/java/com/ead/course/models/CourseModel.java
@@ -1,5 +1,7 @@
 package com.ead.course.models;
 
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -13,31 +15,45 @@ import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "TB_MODULES")
+@Table(name = "TB_COURSES")
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ModuleModel implements Serializable {
+public class CourseModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID moduleId;
+    private UUID courseId;
 
     @Column(nullable = false, length = 150)
-    private String title;
+    private String name;
 
     @Column(nullable = false, length = 250)
     private String description;
+
+    @Column
+    private String imageUrl;
 
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @ManyToOne(optional = false)
-    private CourseModel course;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lastUpdateDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseStatus courseStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+
+    @Column(nullable = false)
+    private UUID userInstructor;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "module")
-    private Set<LessonModel> lessons;
+    @OneToMany(mappedBy = "course")
+    private Set<ModuleModel> modules;
 
 }
