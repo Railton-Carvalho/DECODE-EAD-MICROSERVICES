d1adcffb5daeef3651772029cd3722dd38ad502f
Railton <railtonplayrx@gmail.com>
2026-01-02 19:55:22 -0300

    Add ModuleController, ModuleDto, and ModuleRepository for module management, including CRUD operations and validation. Implement ModuleService methods for module retrieval and manipulation within courses.

--- course/src/main/java/com/ead/course/services/Impl/ModuleServiceImpl.java
+++ course/src/main/java/com/ead/course/services/Impl/ModuleServiceImpl.java
@@ -6,10 +6,14 @@ import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ModuleServiceImpl implements ModuleService {
@@ -22,11 +26,28 @@ public class ModuleServiceImpl implements ModuleService {
 
     @Override
     @Transactional
-    public void deleteModule(ModuleModel moduleModel) {
+    public void delete(ModuleModel moduleModel) {
         List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
         if (!lessonModelList.isEmpty()) {
             lessonRepository.deleteAll(lessonModelList);
         }
         moduleRepository.delete(moduleModel);
     }
+
+    @Override
+    public ModuleModel save(ModuleModel moduleModel) {
+        return moduleRepository.save(moduleModel);
+    }
+
+    @Override
+    public Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId) {
+        return moduleRepository.findModuleIntoCourse(courseId, moduleId);
+    }
+
+    @Override
+    public List<ModuleModel> findAllByCourse(UUID courseId) {
+        return moduleRepository.findAllModulesIntoCourse(courseId);
+    }
 }
+
+
