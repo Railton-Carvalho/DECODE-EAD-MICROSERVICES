79f1b1382f9a4b969941bbc6fd5ee800f15cf18e
Railton <railtonplayrx@gmail.com>
2026-01-02 19:13:12 -0300

    Add CourseController to handle course-related operations including retrieval, creation, update, and deletion of courses. Update CourseDto with additional fields and validation annotations for improved data handling.

---
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -0,0 +1,75 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.services.CourseService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(("/courses"))
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<Page<CourseModel>> getAllCourses(Pageable pageable) {
+        var allCourses = courseService.findAll(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(allCourses);
+    }
+
+
+    @GetMapping("{courseId}")
+    public ResponseEntity<Object> getOneCourse(@RequestParam UUID courseId) {
+        var course = courseService.findById(courseId);
+        if (course.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(course.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found!");
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto) {
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseDto, courseModel);
+        courseModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        courseModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
+    }
+
+    @DeleteMapping("{courseId}")
+    public ResponseEntity<Object> deleteCourse(@PathVariable UUID courseId) {
+        Optional<CourseModel> courseModel = courseService.findById(courseId);
+        if (courseModel.isPresent()) {
+            courseService.delete(courseModel.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Course Deleted successfully.");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found!");
+
+    }
+
+    @PutMapping("{courseId}")
+    public ResponseEntity<Object> updateCourse(@PathVariable UUID courseId, @RequestBody @Valid CourseDto courseDto) {
+        Optional<CourseModel> courseModel = courseService.findById(courseId);
+        if (courseModel.isPresent()) {
+            BeanUtils.copyProperties(courseDto, courseModel.get());
+            courseModel.get().setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            return ResponseEntity.status(HttpStatus.OK).body(courseService.save(courseModel.get()));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found!");
+    }
+
+}
