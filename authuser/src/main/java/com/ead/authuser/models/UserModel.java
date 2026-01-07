eddd0de9f2371ccb887e23373c19f539d2a4a995
Railton <railtonplayrx@gmail.com>
2026-01-06 21:40:29 -0300

    Add UserCourseModel, UserCourseRepository, and UserCourseService to manage user-course relationships. Update UserController to filter users by course ID, enhancing user retrieval capabilities. Include a Set of UserCourseModel in UserModel for better association management.

--- authuser/src/main/java/com/ead/authuser/models/UserModel.java
+++ authuser/src/main/java/com/ead/authuser/models/UserModel.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 
@@ -12,6 +13,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -64,6 +66,10 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy:mm:ss")
     private LocalDateTime lastUpdatedDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<UserCourseModel> usersCourses;
 
     @PrePersist
     protected void onCreate() {
