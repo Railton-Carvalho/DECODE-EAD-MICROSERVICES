93d7cef9d8888ceedfdd8d7c3dd4945fb7cb2a32
Railton <railtonplayrx@gmail.com>
2026-01-11 11:46:20 -0300

    feat: Implement CourseUserService methods for user-course relationship management and update UtilsService for user retrieval URL generation

--- course/src/main/java/com/ead/course/services/CourseUserService.java
+++ course/src/main/java/com/ead/course/services/CourseUserService.java
@@ -1,4 +1,14 @@
 package com.ead.course.services;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface CourseUserService {
+    boolean existsByCourseAndUserId(CourseModel courseModel, @NotNull UUID userId);
+
+    CourseUserModel save(CourseUserModel courseUserModel);
 }
