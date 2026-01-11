4156d646194574edbdf8eba0fcef9b7d754ec5db
Railton <railtonplayrx@gmail.com>
2026-01-11 11:45:33 -0300

    feat: Add AuthUserClient for user retrieval by course ID and SubscriptionDto for user subscription management

--- course/src/main/java/com/ead/course/clients/AuthUserClient.java
+++ course/src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -5,6 +5,7 @@ import com.ead.course.dtos.UserDto;
 import com.ead.course.services.UtilsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -28,9 +29,12 @@ public class CourseClient extends RestTemplate {
     @Autowired
     UtilsService utilsService;
 
+    @Value("${ead.api.authuser}")
+    String REQUEST_URI_AUTHUSER;
+
     public Page<UserDto> getAllUsersByCourse(UUID courseId, Pageable pageable) {
         List<UserDto> searchResult = null;
-        String url = utilsService.createUrl(courseId, pageable);
+        String url = REQUEST_URI_AUTHUSER + utilsService.createUrlGetAllUsersByCourse(courseId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
         try{
